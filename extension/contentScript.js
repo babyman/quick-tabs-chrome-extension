9fc44391d11cd4d00767970e65d467eadbcf7653
babyman <evan.jehu@gmail.com>
2009-12-27 21:06:22 -0500

    tuned up the tab move so that it works for the keyboard and mouse selections

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -2,7 +2,7 @@ document.addEventListener('keyup', keyboardNavigation, false);
 
 function keyboardNavigation(e) {
 
-    if (e.keyCode == 69 && !e.altKey && !e.shiftKey && e.ctrlKey) { // Alt + E
+    if (e.keyCode == 69 && !e.altKey && !e.shiftKey && e.ctrlKey) { // Control + E
         var port = chrome.extension.connect({name: "quicktab"});
         port.postMessage({ command: "openQuickTabs" });
     }
