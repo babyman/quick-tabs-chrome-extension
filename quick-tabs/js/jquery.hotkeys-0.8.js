b20e54b657f0acd4fea3dbb703b74c01ead4d335
babyman <evan.jehu@gmail.com>
2013-11-02 09:23:59 -0400

    Updated hotkeys plugin

--- quick-tabs/js/jquery.hotkeys-0.8.js
+++ quick-tabs/js/jquery.hotkeys-0.8.js
@@ -10,19 +10,25 @@
  * Binny V A, http://www.openjs.com/scripts/events/keyboard_shortcuts/
 */
 
+/*
+ * One small change is: now keys are passed by object { keys: '...' }
+ * Might be useful, when you want to pass some other data to your handler
+ */
+
 (function(jQuery){
 
     jQuery.hotkeys = {
         version: "0.8",
 
         specialKeys: {
-            8: "backspace", 9: "tab", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
+      			8: "backspace", 9: "tab", 10: "return", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
             20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
             37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del",
             96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
             104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/",
             112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8",
-            120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 191: "/", 224: "meta"
+			      120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 186: ";", 191: "/",
+			      220: "\\", 222: "'", 224: "meta"
         },
 
         shiftNums: {
@@ -33,26 +39,32 @@
     };
 
     function keyHandler( handleObj ) {
+         if ( typeof handleObj.data === "string" ) {
+             handleObj.data = { keys: handleObj.data };
+         }
+
         // Only care when a possible input has been specified
-        if ( typeof handleObj.data !== "string" ) {
+		    if ( !handleObj.data || !handleObj.data.keys || typeof handleObj.data.keys !== "string" ) {
             return;
         }
 
         var origHandler = handleObj.handler,
-            keys = handleObj.data.toLowerCase().split(" ");
+    			keys = handleObj.data.keys.toLowerCase().split(" "),
+		    	textAcceptingInputTypes = ["text", "password", "number", "email", "url", "range", "date", "month", "week", "time", "datetime", "datetime-local", "search", "color", "tel"];
 
         handleObj.handler = function( event ) {
             /* [RM]: Deviating from the original here and removing this for QuickTabs
             // Don't fire in text-accepting inputs that we didn't directly bind to
             if ( this !== event.target && (/textarea|select/i.test( event.target.nodeName ) ||
-                 event.target.type === "text") ) {
+				    jQuery.inArray(event.target.type, textAcceptingInputTypes) > -1 ) ) {
                 return;
-            }*/
+			      }
+            */
 
-            // Keypress represents characters, not special keys
-            var special = event.type !== "keypress" && jQuery.hotkeys.specialKeys[ event.which ],
-                character = String.fromCharCode( event.which ).toLowerCase(),
-                key, modif = "", possible = {};
+            var special = jQuery.hotkeys.specialKeys[ event.keyCode ],
+                // character codes are available only in keypress
+                character = event.type === "keypress" && String.fromCharCode( event.which ).toLowerCase(),
+                modif = "", possible = {};
 
             // check combinations (alt|ctrl|shift+anything)
             if ( event.altKey && special !== "alt" ) {
@@ -74,8 +86,9 @@
 
             if ( special ) {
                 possible[ modif + special ] = true;
+      			}
 
-            } else {
+			      if ( character ) {
                 possible[ modif + character ] = true;
                 possible[ modif + jQuery.hotkeys.shiftNums[ character ] ] = true;
 
@@ -97,4 +110,4 @@
         jQuery.event.special[ this ] = { add: keyHandler };
     });
 
-})( jQuery );
+})( this.jQuery );
