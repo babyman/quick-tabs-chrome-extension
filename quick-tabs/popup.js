d76bae9fe23cc8bf845cdf6c019bf8b627b7bdd7
Evan Jehu <evan.jehu@gmail.com>
2016-03-20 23:01:42 -0400

    fixes #124, Slack and Trello have no favicons

--- quick-tabs/popup.js
+++ quick-tabs/popup.js
@@ -641,7 +641,8 @@ function encodeHTMLSource(str) {
 function tabImage(tab) {
   if(tab.audible) {
     return "/assets/noisy.png"
-  } else if (tab.favIconUrl && /^https?:\/\/.*/.exec(tab.favIconUrl)) {
+  } else if (tab.favIconUrl && (startsWith(tab.favIconUrl, "data:") || /^https?:\/\/.*/.exec(tab.favIconUrl))) {
+    // if the favicon is a valid URL or embedded data return that
     return tab.favIconUrl;
   } else if(/^chrome:\/\/extensions\/.*/.exec(tab.url)) {
     return "/assets/chrome-extensions-icon.png";
