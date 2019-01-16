54d76fc86536e84f56366f5292900394dff4f51b
doctorgester <doctorgester@gmail.com>
2019-01-16 08:26:58 +0300

    Removed extra spaces

--- quick-tabs/popup.js
+++ quick-tabs/popup.js
@@ -571,7 +571,7 @@ function encodeHTMLSource(str) {
   var encodeHTMLRules = {"&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;', "\v": '<b>', "\b": '</b>'},
       matchHTML = /&(?!#?\w+;)|<|>|"|'|\/|[\v]|[\b]/g;
   return str ? str.replace(matchHTML, function(m) {
-      return encodeHTMLRules[m] || m;
+    return encodeHTMLRules[m] || m;
   }) : str;
 }
 
