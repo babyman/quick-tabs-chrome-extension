b9e6cea775565e011f3c5006dfc12bc4832268c8
Evan Jehu <evan.jehu@gmail.com>
2020-01-17 08:40:14 -0500

    fix error when trying to scroll to missing element, fixes #298

--- quick-tabs/popup.js
+++ quick-tabs/popup.js
@@ -153,22 +153,22 @@ function isFocusSet() {
 function scrollToFocus() {
   const element = entryWithFocus();
 
-  const offset = element.offset().top;
-  const elementHeight = element.outerHeight(true) * 2;
+  // make sure we have an element to scroll to
+  if(element.length > 0) {
+    const offset = element.offset().top;
+    const elementHeight = element.outerHeight(true) * 2;
 
-  const visible_area_start = $(window).scrollTop();
-  const visible_area_end = visible_area_start + window.innerHeight;
+    const visible_area_start = $(window).scrollTop();
+    const visible_area_end = visible_area_start + window.innerHeight;
 
-  if (offset < visible_area_start + elementHeight) {
-    // scrolling up
-    window.scroll({top: offset - elementHeight, left: 0, behavior: 'smooth'});
-    return false;
-  } else if (offset > visible_area_end - elementHeight) {
-    // scrolling down
-    window.scroll({top: offset - window.innerHeight + elementHeight, left: 0, behavior: 'smooth'});
-    return false;
+    if (offset < visible_area_start + elementHeight) {
+      // scrolling up
+      window.scroll({top: offset - elementHeight, left: 0, behavior: 'smooth'});
+    } else if (offset > visible_area_end - elementHeight) {
+      // scrolling down
+      window.scroll({top: offset - window.innerHeight + elementHeight, left: 0, behavior: 'smooth'});
+    }
   }
-  return true;
 }
 
 function focus(elem) {
